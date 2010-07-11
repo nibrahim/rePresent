eb5778feecbd018e2d131218916accf477cdd09d
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-07-12 04:36:37 +0530

    Sample presentation added

---
+++ sample-presentation.el
@@ -0,0 +1,20 @@
+;;;; This is a sample presentation file for the rePresent.el loader
+
+(setq presentation
+      '(
+	(slide :title "Sample slide #1"
+	       :bullets ("Bullet 1"
+			 "Bullet 2"
+			 "Bullet 3"
+			 "Bullet 4"))
+
+	(slide :title "Sample slide #2"
+	       :bullets ("Bullet a"
+			 "Bullet b"
+			 "Bullet c"
+			 "Bullet d"))))
+
+	
+
+
+	     
